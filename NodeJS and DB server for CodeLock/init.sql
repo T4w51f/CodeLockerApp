3f84e0899cf1038332cce6fbc5a43a4b513d9f03
t4w51f <tawsifhasan1998@yahoo.com>
2019-12-07 18:17:27 -0800

    Heroku

--- NodeJS and DB server for CodeLock/init.sql
+++ NodeJS and DB server for CodeLock/init.sql
@@ -1,27 +1,23 @@
 CREATE TABLE public.app_user
 (
     id integer NOT NULL,
-    user_id integer NOT NULL,
     name character varying(255) COLLATE pg_catalog."default",
     email character varying(255) COLLATE pg_catalog."default",
     password character varying(255) COLLATE pg_catalog."default",
     created_at timestamp with time zone,
     updated_at timestamp with time zone,
     username character varying(255) COLLATE pg_catalog."default",
+    user_id uuid,
     CONSTRAINT app_user_pkey PRIMARY KEY (id)
 );
 
 CREATE TABLE public.platform
 (
     id integer NOT NULL,
-    user_id uuid NOT NULL,
     app_name character varying(255) COLLATE pg_catalog."default",
     username character varying(255) COLLATE pg_catalog."default",
     password character varying(255) COLLATE pg_catalog."default",
     created_at timestamp with time zone,
     updated_at timestamp with time zone,
-    CONSTRAINT platform_pkey PRIMARY KEY (id)
-);
-
-INSERT INTO app_user (id, user_id, name, email, password) 
-VALUES ('1', '1f44f1ae-ecab-11e9-81b4-2a2ae2dbcce4', 'Tawsif Hasan', 'th@gmail.com', 'testpassword');
+    user_id uuid
+)
